0c45db91e17a3d6ab70e3db640290b8e4778bdc6
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-09-06 17:34:27 +0530

    [Prasoon] Added use case 1 for entering the elements in full BST

--- Binary_Tree/Program.cs
+++ Binary_Tree/Program.cs
@@ -75,12 +75,29 @@ namespace Binary_Tree
             Console.WriteLine("Binary Search tree");
             BinarySearchTree B = new BinarySearchTree();
             Console.WriteLine("Enter 1 for entering the elements");
+            Console.WriteLine("Enter 2 for seening the elements");
             int ans=Convert.ToInt32(Console.ReadLine());
             switch(ans)
             {
                 case 1: B.insert(B.root, 56); //UC1 
                         B.insert(B.root, 30);
                         B.insert(B.root, 70);
+                        break;
+                case 2:
+                    B.insert(B.root, 56); //UC2
+                    B.insert(B.root, 30);
+                    B.insert(B.root, 70);
+                    B.insert(B.root, 22);
+                    B.insert(B.root, 40);
+                    B.insert(B.root, 60);
+                    B.insert(B.root, 95);
+                    B.insert(B.root, 11);
+                    B.insert(B.root, 65);
+                    B.insert(B.root, 3);
+                    B.insert(B.root, 16);
+                    B.insert(B.root, 63);
+                    B.insert(B.root, 67);
+                    B.inorder(B.root);
                         break;
                 default: Console.WriteLine("Null Value"); break;
             }
