d31614d51460941d8b0de74678ed1007b954429e
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-09-06 19:03:20 +0530

    [Prasoon] Added Searching element in BST

--- Binary_Tree/Program.cs
+++ Binary_Tree/Program.cs
@@ -66,9 +66,39 @@ namespace Binary_Tree
             if (temproot!=null)
             {
                 inorder(temproot.left);
-                Console.WriteLine("The element is: "+temproot.element);
+                Console.Write(" "+temproot.element);
                 inorder(temproot.right);
             }
+        }
+        public void preorder(Node temproot)
+        {
+            if(temproot!=null)
+            {
+                Console.Write(" "+temproot.element);
+                preorder(temproot.left);
+                preorder(temproot.right);
+            }
+        }
+
+        public bool search(int key)
+        {
+            Node temproot = root;
+            while(temproot!=null)
+            {
+                if (key == temproot.element)
+                {
+                    return true;
+                }
+                else if (key < temproot.element)
+                {
+                    temproot = temproot.left;
+                }
+                else if(key> temproot.element)
+                {
+                    temproot= temproot.right;
+                }
+            }
+            return false;
         }
         static void Main(string[] args)
         {
@@ -76,6 +106,7 @@ namespace Binary_Tree
             BinarySearchTree B = new BinarySearchTree();
             Console.WriteLine("Enter 1 for entering the elements");
             Console.WriteLine("Enter 2 for seening the elements");
+            Console.WriteLine("Enter 3 for searching an element");
             int ans=Convert.ToInt32(Console.ReadLine());
             switch(ans)
             {
@@ -97,8 +128,33 @@ namespace Binary_Tree
                     B.insert(B.root, 16);
                     B.insert(B.root, 63);
                     B.insert(B.root, 67);
+                    Console.WriteLine("Inorder Traversal");
                     B.inorder(B.root);
-                        break;
+                    Console.WriteLine();
+                    Console.WriteLine("PReOder traversal");
+                    B.preorder(B.root);
+                    Console.WriteLine();
+                    break;
+                case 3:
+                    B.insert(B.root, 56); //UC2
+                    B.insert(B.root, 30);
+                    B.insert(B.root, 70);
+                    B.insert(B.root, 22);
+                    B.insert(B.root, 40);
+                    B.insert(B.root, 60);
+                    B.insert(B.root, 95);
+                    B.insert(B.root, 11);
+                    B.insert(B.root, 65);
+                    B.insert(B.root, 3);
+                    B.insert(B.root, 16);
+                    B.insert(B.root, 63);
+                    B.insert(B.root, 67);
+                    Console.WriteLine("Inorder Traversal");
+                    B.inorder(B.root);
+                    Console.WriteLine();
+                    Console.WriteLine("Enter the element to search");
+                    Console.WriteLine("Search result :"+B.search(100)); 
+                    break;
                 default: Console.WriteLine("Null Value"); break;
             }
         }
